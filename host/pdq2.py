c7ba8a863387ae329ef4f250e604ce47f2d368f1
Robert Jordens <rj@m-labs.hk>
2016-11-04 12:52:25 +0100

    cleanup crc

--- host/pdq2.py
+++ host/pdq2.py
@@ -45,25 +45,43 @@ def discrete_compensate(c):
 
 
 class CRC:
-    def __init__(self, width=8, poly=0x07):
-        self.poly = poly
-        self.width = width
-        self._table = [self._byte(i) for i in range(1 << width)]
+    """Generic and simple table driven CRC calculator.
 
-    def _byte(self, i):
-        for j in range(self.width):
+    This implementation is:
+
+        * MSB first data
+        * "un-reversed" full polynomial (i.e. starts with 0x1)
+        * no initial complement
+        * no final complement
+
+    Handle any variation on those details outside this class.
+
+    >>> r = CRC(0x1814141AB)(b"123456789")  # crc-32q
+    >>> assert r == 0x3010BF7F, hex(r)
+    """
+    def __init__(self, poly=0x107, data_width=8):
+        self.poly = poly
+        self.crc_width = poly.bit_length() - 1
+        self.data_width = data_width
+        self._table = [self._one(i << self.crc_width - data_width)
+                       for i in range(1 << data_width)]
+
+    def _one(self, i):
+        for j in range(self.data_width):
             i <<= 1
-            if i & (1 << self.width):
+            if i & 1 << self.crc_width:
                 i ^= self.poly
-        return i & ((1 << self.width) - 1)
+        return i
 
     def __call__(self, msg, crc=0):
-        for b in msg:
-            crc = self._table[b ^ crc]
+        for data in msg:
+            p = data ^ crc >> self.crc_width - self.data_width
+            q = crc << self.data_width & (1 << self.crc_width) - 1
+            crc = self._table[p] ^ q
         return crc
 
 
-crc8 = CRC(width=8, poly=0x07)
+crc8 = CRC(0x107)
 
 
 class Segment:
