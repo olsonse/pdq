8abafd3c6d91ef14cfdd8aa554008e2491045a8a
Robert Jordens <rj@m-labs.hk>
2016-11-05 19:06:18 +0100

    test: revive dac test

--- testbench/dac.py
+++ testbench/dac.py
@@ -15,11 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with pdq2.  If not, see <http://www.gnu.org/licenses/>.
 
+from io import BytesIO
+
 from migen import *
 
 from matplotlib import pyplot as plt
 import numpy as np
-from scipy import interpolate
 
 from gateware.dac import Dac
 from host import pdq2
@@ -29,23 +30,70 @@ class TB(Module):
     def __init__(self, mem=None):
         self.submodules.dac = Dac()
         if mem is not None:
-            self.dac.parser.mem.init = mem
+            self.dac.parser.mem.init = [int(i) for i in mem]
         self.outputs = []
         self.dac.parser.frame.reset = 0
 
-    def do_simulation(self, selfp):
-        self.outputs.append(selfp.dac.out.data)
-        if selfp.simulator.cycle_counter == 5:
-            selfp.dac.parser.start = 1
-            selfp.dac.parser.arm = 1
-            selfp.dac.out.arm = 1
-        elif selfp.simulator.cycle_counter == 20:
-            selfp.dac.out.trigger = 1
-        elif selfp.simulator.cycle_counter == 21:
-            selfp.dac.out.trigger = 0
-        if selfp.dac.out.sink.ack and selfp.dac.out.sink.stb:
-            print("cycle {} data {}".format(
-                selfp.simulator.cycle_counter, selfp.dac.out.data))
+    def run(self, ncycles):
+        for i in range(ncycles):
+            self.outputs.append((yield self.dac.out.data))
+            if i == 5:
+                yield self.dac.parser.start.eq(1)
+                yield self.dac.parser.arm.eq(1)
+                yield self.dac.out.arm.eq(1)
+            elif i == 20:
+                yield self.dac.out.trigger.eq(1)
+            elif i == 21:
+                yield self.dac.out.trigger.eq(0)
+            yield
+            if (yield self.dac.out.sink.ack) and \
+                    (yield self.dac.out.sink.stb):
+                print("cycle {} data {}".format(
+                    i, (yield self.dac.out.data)))
+
+
+_test_program = [
+    [
+        {
+            "trigger": True,
+            "duration": 20,
+            "channel_data": [
+                {"bias": {"amplitude": [0, 0, 2e-3]}},
+                {"bias": {"amplitude": [1, 0, -7.5e-3, 7.5e-4]}},
+                {"dds": {
+                    "amplitude": [0, 0, 4e-3, 0],
+                    "phase": [.25, .025],
+                }},
+            ],
+        },
+        {
+            "duration": 40,
+            "channel_data": [
+                {"bias": {"amplitude": [.4, .04, -2e-3]}},
+                {
+                    "bias": {"amplitude": [.5]},
+                    "silence": True,
+                },
+                {"dds": {
+                    "amplitude": [.8, .08, -4e-3, 0],
+                    "phase": [.25, .025, .02/40],
+                    "clear": True,
+                }},
+            ],
+        },
+        {
+            "duration": 20,
+            "channel_data": [
+                {"bias": {"amplitude": [.4, -.04, 2e-3]}},
+                {"bias": {"amplitude": [.5, 0, -7.5e-3, 7.5e-4]}},
+                {"dds": {
+                    "amplitude": [.8, -.08, 4e-3, 0],
+                    "phase": [-.25],
+                }},
+            ],
+        },
+    ]
+]
 
 
 def _main():
@@ -55,44 +103,11 @@ def _main():
     # from migen.fhdl import verilog
     # print(verilog.convert(Dac()))
 
-    t = np.arange(7) * 18
-    t = t.astype(np.int32)
-    v = (1 << 14)*(1 - np.cos(t/t[-1]*2*np.pi))/2
-    v = v.astype(np.int16)
-    k = 3
-    p = pdq2.Pdq2(dev="dummy")
-    c = p.channels[0]
-    s = c.new_segment()
-    for i, (ti, vi) in enumerate(zip(t, v)):
-        # TODO: order
-        s.bias([vi], duration=ti, trigger=(i == 0))
-    s.dds(2*t, (v/c.cordic_gain).astype(np.int16),
-          0*t + (1 << 14), (t/t[-1]*(1 << 13)).astype(np.int16),
-          first=dict(trigger=False))
-    mem = c.serialize()
+    p = pdq2.Pdq2(dev=BytesIO())
+    p.program(_test_program)
+    mem = p.channels[0].serialize()
     tb = TB(list(np.fromstring(mem, "<u2")))
-    run_simulation(tb, ncycles=400, vcd_name="dac.vcd")
-
-    plt.plot(t, v, "xk")
-
-    sp = interpolate.splrep(t, v, k=k, s=0)
-    tt = np.arange(t[-1])
-    vv = interpolate.splev(tt, sp)
-    plt.plot(tt, vv, "+g")
-
-    vv1 = []
-    widths = np.array([0, 1, 2, 2])
-    dv = pdq2.Segment.interpolate(t, v, k, t[:-1], widths)
-    dv = dv/2**(16*widths)
-
-    for i, (ti, dvi) in enumerate(zip(t, dv)):
-        dt = 0
-        while ti + dt < t[i + 1]:
-            dt += 1
-            vv1.append(dvi[0])
-            for ki in range(k):
-                dvi[ki] += dvi[ki + 1]
-    plt.step(tt + 1, vv1, "-g")
+    run_simulation(tb, tb.run(400), vcd_name="dac.vcd")
 
     out = np.array(tb.outputs, np.uint16).view(np.int16)
     plt.step(np.arange(len(out)) - 22, out, "-r")
