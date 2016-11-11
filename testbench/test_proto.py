df047eb09fbd55e23f8fe259bd58f7f8ae59114e
Robert Jordens <rj@m-labs.hk>
2016-11-11 12:40:24 +0100

    test_proto: adapt

--- testbench/test_proto.py
+++ testbench/test_proto.py
@@ -64,32 +64,32 @@ class TB(Module):
         assert r == 0x0001, hex(r)
         yield from self.seq([
             (1 << 7) | (0b0101 << 3) | (1 << 2) | (0 << 0),
-            0x03, 0x20, 0x02, 0x00])
-        r = (yield self.mems[0][3])
+            0x02, 0x20, 0x02, 0x00])
+        r = (yield self.mems[0][1])
         assert r == 0x0002, hex(r)
         yield from self.seq([
             (1 << 7) | (0b0101 << 3) | (1 << 2) | (2 << 0),
-            0x01, 0x30, 0x0f, 0x10])
-        r = (yield self.mems[2][1])
+            0x04, 0x30, 0x0f, 0x10])
+        r = (yield self.mems[2][2])
         assert r == 0x100f, hex(r)
 
         # test read
         r = (yield from self.seq([
             (0 << 7) | (0b0101 << 3) | (1 << 2) | (2 << 0),
-            0x01, 0x30, 0x00, 0x00]))
+            0x04, 0x30, 0x00, 0x00]))
         assert r == [0x0f, 0x10], r
 
         # test multi write
         yield from self.seq([
             (1 << 7) | (0b0101 << 3) | (1 << 2) | (0 << 0),
-            0x01, 0x00, 0x01, 0x10, 0x02, 0x20, 0x03, 0x30])
+            0x02, 0x00, 0x01, 0x10, 0x02, 0x20, 0x03, 0x30])
         r = yield from [(yield self.mems[0][i]) for i in range(1, 4)]
         assert r == [0x1001, 0x2002, 0x3003], r
 
         # test multi read
         r = yield from self.seq([
             (0 << 7) | (0b0101 << 3) | (1 << 2) | (0 << 0),
-            0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         assert r == [0x01, 0x10, 0x02, 0x20, 0x03, 0x30], r
 
 
