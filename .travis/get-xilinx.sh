2f3b937530fe149de27a301910f5075b83a354ce
Robert Jordens <rj@m-labs.hk>
2017-06-28 21:20:46 +0200

    travis: update build urls

--- .travis/get-xilinx.sh
+++ .travis/get-xilinx.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wget http://www.phys.ethz.ch/~robertjo/xilinx_ise_14.7_s3_s6.tar.gz.gpg
+wget http://m-labs.hk/build/xilinx_ise_14.7_s3_s6.tar.gz.gpg
 echo "$secret" | gpg --passphrase-fd 0 xilinx_ise_14.7_s3_s6.tar.gz.gpg
 tar -xzf xilinx_ise_14.7_s3_s6.tar.gz
-wget http://www.phys.ethz.ch/~robertjo/xilinx_webpack.lic.gpg
+wget http://m-labs.hk/build/xilinx_webpack.lic.gpg
 echo "$secret" | gpg --passphrase-fd 0 xilinx_webpack.lic.gpg
 mkdir ~/.Xilinx
 mv xilinx_webpack.lic ~/.Xilinx/Xilinx.lic
