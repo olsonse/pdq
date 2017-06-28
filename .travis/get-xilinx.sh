a8aa6046fd1edb4743ed64370c50a9be6844ebbf
Robert Jordens <rj@m-labs.hk>
2017-06-28 21:50:18 +0200

    travis: fix xilinx install

--- .travis/get-xilinx.sh
+++ .travis/get-xilinx.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-mkdir -p xilinx
-cd xilinx
+cd cache
 wget -N http://m-labs.hk/build/xilinx_ise_14.7_s3_s6.tar.gz.gpg
-echo "$secret" | gpg --passphrase-fd 0 xilinx_ise_14.7_s3_s6.tar.gz.gpg
-tar -xzf xilinx_ise_14.7_s3_s6.tar.gz
 wget -N http://m-labs.hk/build/xilinx_webpack.lic.gpg
-echo "$secret" | gpg --passphrase-fd 0 xilinx_webpack.lic.gpg
+
+cd ..
+echo "$secret" \
+	| gpg --passphrase-fd 0 -d cache/xilinx_ise_14.7_s3_s6.tar.gz.gpg \
+	| tar xz
 mkdir ~/.Xilinx
-mv xilinx_webpack.lic ~/.Xilinx/Xilinx.lic
+echo "$secret" \
+	| gpg --passphrase-fd 0 -d cache/xilinx_webpack.lic.gpg \
+	> ~/.Xilinx/Xilinx.lic
